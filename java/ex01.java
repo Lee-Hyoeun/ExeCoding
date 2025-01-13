faf033dcd756013361801f2a8a47c82998b506ee
Lee-Hyoeun <gydms0136@gmail.com>
2025-01-14 07:40:51 +0900

    Create ex01.java

---
+++ java/ex01.java
@@ -0,0 +1,13 @@
+publci class {
+    public static void main(String args[]{
+      int n = 10;
+      n+=2;
+      n-=3;  
+      n*=5;
+      n/=7;
+      n%=11;
+      System.out.println(n);
+    }
+}
+
+//6 
