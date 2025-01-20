eae6817ecccfccbb14581ffcffe8087b6c743281
Lee-Hyoeun <gydms0136@gmail.com>
2025-01-20 23:13:51 +0900

    Create ex02.java

---
+++ java/ex02.java
@@ -0,0 +1,17 @@
+//산 17년 1회, 20년 2회
+public class Soojebi{
+  public static void main(String args[]){
+    int[] a = {1,1,2,1,2,3,1,2,3,4};
+    int b = 0;
+
+  for(int i=0; i<a.length; i++){
+    if(a[i] == 3){
+      b++;
+    }
+  }
+
+  System.out.println(b);
+  }
+}
+
+//o
