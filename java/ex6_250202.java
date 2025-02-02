ff19450fdd6aad3ae81272270b4b28f540e8b56f
Lee-Hyoeun <gydms0136@gmail.com>
2025-02-02 23:54:03 +0900

    Create ex6_250202.java

---
+++ java/ex6_250202.java
@@ -0,0 +1,13 @@
+//20년 1회
+
+public class Soojebi{
+  public static void main(String args[]){
+    int i;
+    int []a = {0,1,2,3};
+    for(i=0; i<4; i++){
+      System.out.print(a[i] + "");
+    }
+  }
+}
+
+//o
