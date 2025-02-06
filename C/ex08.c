46a87d211074c186195114bfa7b09f80978094e0
Lee-Hyoeun <gydms0136@gmail.com>
2025-02-07 07:11:16 +0900

    Update ex08.c

--- C/ex08.c
+++ C/ex08.c
@@ -1,5 +1,16 @@
+// 산18년 2회, 19년 1회
+
 #include<stdio.h>
 
+void main(){
+  int i, j;
 
+  for(i=2; i<=3; i++){
+    for(j=1; j<=4; j++){
+      printf("%d * %d = %d₩n", i, j, i*j);
 
+  }
+ }
+}
 
+//o
